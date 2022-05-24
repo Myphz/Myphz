fd432d48c66c16048911d5c7a6b46b08a98cee36
Myphz <danielscanu45@outlook.it>
2022-05-24 17:35:32 +0200

    feature: add fade in text after typing animation; general style fixes

--- src/components/TypeText/index.tsx
+++ src/components/TypeText/index.tsx
@@ -5,20 +5,26 @@ interface Props {
   text: string,
   delay: number,
   speed: number,
+  blinkDelay: number,
   charDelete?: number,
   replace?: string
 };
 
-export const TypeText: React.FC<Props> = ({ text, delay, speed, charDelete, replace }) => {
+export const TypeText: React.FC<Props> = ({ text, delay, speed, blinkDelay, charDelete, replace }) => {
   const span = useRef<HTMLSpanElement>(null);
   const [typingText, setTypingText] = useState("");
   let interval: number;
   let i = 0;
 
-  // Remove the blinking animation after 1 second
-  function removeBlinking() {
-    setTimeout(() => span.current?.classList?.remove("blinking-animation"), 1000);
+  // Add blinking animation
+  function addBlinking() {
+    span.current?.classList?.add("blinking-animation");
   }
+
+  // Remove the blinking animation after 0.5 second
+  function removeBlinking() {
+    setTimeout(() => span.current?.classList?.remove("blinking-animation"), 500);
+  };
 
   // Add the letter at index i of the parameter to typingText. Stop if done
   function addLetter(text: string): void {
@@ -27,7 +33,8 @@ export const TypeText: React.FC<Props> = ({ text, delay, speed, charDelete, repl
     if (i === text.length-1) {
       clearInterval(interval);
       if (!charDelete) return removeBlinking();
-      setTimeout(() => interval = setInterval(deleteLetter, speed*1.5), 1000);
+      // Start deleting letters after 1 second, 50% faster than writing
+      setTimeout(() => interval = setInterval(deleteLetter, speed/2), 1000);
     }
     i++;
   };
@@ -43,15 +50,15 @@ export const TypeText: React.FC<Props> = ({ text, delay, speed, charDelete, repl
       setTimeout(() => {
         i = 0;
         interval = setInterval(() => addLetter(replace), speed);
-      }, 1000);
+      }, 500);
     }
   };
 
   // On mount hook
   useEffect(() => {
+    setTimeout(addBlinking, blinkDelay);
     setTimeout(() => {
       interval = setInterval(() => addLetter(text), speed);
-      span.current?.classList?.add("blinking-animation");
     }, delay);
   }, []);
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,18 +7,24 @@ export const Home: React.FC = () => {
     <main className="align-center noselect">
       <section>
         <h1 className="main-text color-primary">
-          <TypeText text="Daniel" delay={1000} speed={120} />
+          <TypeText text="Daniel" delay={1000} speed={120} blinkDelay={0} />
         </h1>
 
-        <h2 className="thin secondary-text color-secondary">
+        <h3 className="thin secondary-text color-secondary">
           <TypeText
             text="PROGRAMMER AND PROFESSIONAL GOOGLE SEARCHER"
             delay={2800}
-            speed={60}
+            speed={100}
+            blinkDelay={2300}
             charDelete={27}
             replace="WEB DEVELOPER"
           />
-        </h2>
+        </h3>
+        <p className="h4 info">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Fusce sit amet massa sed tellus rutrum auctor. Vestibulum a bibendum erat.
+          Donec aliquet egestas nisl.
+        </p>
       </section>
     </main>
   );
