6ca338c121fc7dc0cafcdafbaf954949e76882cc
Myphz <danielscanu45@outlook.it>
2022-05-25 19:58:11 +0200

    feature: implement Contact page design

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./style.sass";
 
 interface Props {
-  children: React.ReactNode
+  children: React.ReactNode,
+  submit?: boolean
 };
 
-export const Button: React.FC<Props> = ({ children }) => {
+export const Button: React.FC<Props> = ({ children, submit }) => {
   return (
-    <button className="button">
+    <button className="button" type={submit ? "submit" : undefined}>
       { children }
     </button>
   );
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Button } from "../../components/Button";
+import { TypeText } from "../../components/TypeText";
+import "./style.sass"
 
 export const Contact: React.FC = () => {
   return (
-    <main>
-      <div>test</div>
+    <main className="justify-center flex-column">
+      <h1 className="color-primary font-title">
+        <TypeText text="Contact" delay={200} speed={100} blinkDelay={50} />
+      </h1>
+      <section className="input-section">
+        <h4>My inbox is always open for new opportunities.<br />
+        Feel free to contact me for any inquiries you may have!</h4>
+        <form className="input-form h4">
+          <input type="text" placeholder="Email" autoComplete="email" spellCheck="false" autoCapitalize="none" />
+          <input type="text" placeholder="Subject" spellCheck="false" autoCapitalize="none" />
+          <textarea placeholder="Message" />
+          <Button submit>Send Message</Button>
+        </form>
+      </section>
     </main>
   );
 }
