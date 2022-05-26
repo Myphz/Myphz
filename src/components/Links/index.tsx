77542c58dd2c032f55a4185450cfdecfd9258d49
Myphz <danielscanu45@outlook.it>
2022-05-27 00:24:22 +0200

    feature: add dynamic classes to links

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.sass";
 
 export const Links: React.FC = () => {
@@ -6,16 +6,19 @@ export const Links: React.FC = () => {
   let navbar: Element;
   let root: Element;
 
-  let currentPageIndex = 0;
+  let [currentPageIndex, setCurrentPageIndex] = useState(0);
   let scrollTimeout = false;
 
-  function redirect(i: number) {
-    if (i < 0 || i >= elements.length || scrollTimeout) return;
+  function redirect(toAdd: number) {
+    const newIdx = currentPageIndex + toAdd;
+    if (newIdx < 0 || newIdx >= elements.length || scrollTimeout) return;
     // Prevent scrolling for 100ms
     scrollTimeout = true;
     setTimeout(() => scrollTimeout = false, 100);
-    currentPageIndex = i;
-    root.scrollTo(0, root.scrollTop + elements[i].getBoundingClientRect().top - navbar.clientHeight);
+
+    root.scrollTo(0, root.scrollTop + elements[currentPageIndex + toAdd].getBoundingClientRect().top - navbar.clientHeight);
+    setCurrentPageIndex(currentPageIndex => currentPageIndex + toAdd);
+    currentPageIndex = newIdx;
   }
 
   useEffect(() => {
@@ -26,8 +29,8 @@ export const Links: React.FC = () => {
     root.addEventListener("wheel", (e: Event) => {
       e.preventDefault();
       const { deltaY } = e as WheelEvent;
-      if (deltaY < 0) redirect(currentPageIndex-1);
-      else redirect(currentPageIndex+1);
+      if (deltaY < 0) redirect(-1);
+      else redirect(1);
     });
   }, []);
 
@@ -35,11 +38,21 @@ export const Links: React.FC = () => {
     <aside className="links-aside">
       <nav tabIndex={1} className="navbar">
         <ol start={0} className="h6 text-secondary" onClick={() => (document.activeElement as HTMLElement).blur()}>
-          <li style={{"--order": 1} as React.CSSProperties}><a onClick={() => redirect(0)}>ABOUT</a></li>
-          <li style={{"--order": 2} as React.CSSProperties}><a onClick={() => redirect(1)}>SKILLS</a></li>
-          <li style={{"--order": 3} as React.CSSProperties}><a onClick={() => redirect(2)}>PROJECTS</a></li>
-          <li style={{"--order": 4} as React.CSSProperties}><a onClick={() => redirect(3)}>CONTACT</a></li>
-          <li style={{"--order": 5} as React.CSSProperties}><a>RESUME</a></li>
+          <li style={{"--order": 1} as React.CSSProperties}>
+            <a className={currentPageIndex === 0 ? "active" : ""} onClick={() => redirect(0)}>ABOUT</a>
+          </li>
+          <li style={{"--order": 2} as React.CSSProperties}>
+            <a className={currentPageIndex === 1 ? "active" : ""} onClick={() => redirect(1)}>SKILLS</a>
+          </li>
+          <li style={{"--order": 3} as React.CSSProperties}>
+            <a className={currentPageIndex === 2 ? "active" : ""} onClick={() => redirect(2)}>PROJECTS</a>
+          </li>
+          <li style={{"--order": 4} as React.CSSProperties}>
+            <a className={currentPageIndex === 3 ? "active" : ""} onClick={() => redirect(3)}>CONTACT</a>
+          </li>
+          <li style={{"--order": 5} as React.CSSProperties}>
+            <a>RESUME</a>
+          </li>
         </ol>
       </nav>
     </aside>
