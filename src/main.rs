10bb60fb9d9c8122d9305195e5051447d6b3da9a
Myphz <danielscanu45@outlook.it>
2022-05-28 18:32:59 +0200

    feature: add basic JSON formatting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 #[macro_use] extern crate rocket;
+use rocket::serde::{Deserialize, json::Json};
 
-#[get("/")]
-fn index() -> &'static str {
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Test {
+    email: String,
+    subject: String,
+    message: String
+}
+
+#[post("/", data = "<data>")]
+fn index(data: Json<Test>) -> &'static str {
+    let Test { email, subject, message } = data.into_inner();
+    println!("{} {} {}", email, subject, message);
     "Hello, world!"
 }
 
