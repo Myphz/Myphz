6147272cc1c6e9d103211de6267212f090257ef3
Myphz <danielscanu45@outlook.it>
2022-05-28 19:44:02 +0200

    feature: initialize send_email structure; add `email` mod

---
+++ src/email.rs
@@ -0,0 +1,5 @@
+use crate::Params;
+
+pub fn send_email(Params { email, subject, message}: Params) -> bool {
+    return true;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #[macro_use] extern crate rocket;
+
 use rocket::serde::{json::Json, Deserialize, Serialize};
 use rocket::http::Status;
 use rocket::response::status;
 use validator::Validate;
 
+mod email;
+
 #[derive(Deserialize, Validate)]
-struct Params {
+pub struct Params {
     #[validate(email)]
     email: String,
     subject: String,
@@ -23,11 +26,25 @@ fn index(data: Json<Params>) -> status::Custom<Json<Response>> {
     let fields = data.into_inner();
     let is_valid = fields.validate();
     if is_valid.is_err() {
-        return status::Custom(Status::BadRequest, Json(Response { success: false, message: String::from("Invalid")}));
+        return status::Custom(Status::BadRequest, Json(Response { success: false, message: String::from("Invalid email. Please retry.")}));
+    };
+
+    let success = email::send_email(fields);
+    return if success {
+        status::Custom(
+            Status::Ok,
+            Json(
+                Response { success: true, message: String::from("Email sent! I'll reply back soon ;)")}
+            )
+        )
+    } else {
+        status::Custom(
+            Status::InternalServerError,
+            Json(
+                Response { success: false, message: String::from("Error sending the email. Please send the email manually.")}
+            )
+        )
     }
-    let Params { email, subject, message } = fields;
-    println!("{:?}, {} {} {}", is_valid, email, subject, message);
-    return status::Custom(Status::Ok, Json(Response { success: true, message: String::from("Invalid")}));
 }
 
 #[launch]
