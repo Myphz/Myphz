12c9f58812691672b8ef405906f9a2e4160b3f37
Myphz <danielkeq@gmail.com>
2023-11-04 19:36:33 +0100

    feat: initialize design system, remove vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
-import './assets/main.css'
+import "./assets/main.css"
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue"
+import App from "./App.vue"
 
 const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.mount("#app")
