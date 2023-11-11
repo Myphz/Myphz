1c8c6b7314645656659ed3ddcef5ff9f9f6dfd1d
Myphz <danielkeq@gmail.com>
2023-11-11 12:02:24 +0100

    chore: format; add ogl initialization

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import "./assets/main.css"
+import "./assets/main.css";
 
-import { ViteSSG } from "vite-ssg/single-page"
-import App from "./App.vue"
+import { ViteSSG } from "vite-ssg/single-page";
+import App from "./App.vue";
 
 // @ts-nocheck
-export const createApp = ViteSSG(App)
+export const createApp = ViteSSG(App);
