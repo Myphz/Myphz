cec32b0ff41c0efd0250a511819c7556b32d0e00
Myphz <danielkeq@gmail.com>
2023-11-11 12:32:11 +0100

    chore: initialize landing

--- src/components/PageWrapper.vue
+++ src/components/PageWrapper.vue
@@ -1,5 +1,14 @@
 <template>
-  <section class="full-page-height">we</section>
+  <section class="full-page-height wrapper">
+    <slot />
+  </section>
 </template>
 
 <script setup lang="ts"></script>
+
+<style scoped>
+.wrapper {
+  margin: 0 10rem;
+  padding: 3rem 0;
+}
+</style>
--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,9 +1,23 @@
 <template>
   <PageWrapper>
-    <div>ciao</div>
+    <div class="container-centered">
+      <h1 class="h1 main-text">Daniel</h1>
+    </div>
   </PageWrapper>
 </template>
 
 <script setup lang="ts">
 import PageWrapper from "@/components/PageWrapper.vue";
 </script>
+
+<style scoped>
+.container-centered {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.main-text {
+  color: var(--primary);
+}
+</style>
