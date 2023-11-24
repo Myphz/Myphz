26496743d4090448d7c873370096e85ae9becbd9
Myphz <danielkeq@gmail.com>
2023-11-24 18:31:17 +0100

    feat: add images

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -38,22 +38,19 @@ const PROJECTS = [
     name: "Trips!",
     redirect: "/",
     description: "Trips is good",
-    image:
-      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+    image: "/trips.webp"
   },
   {
     name: "Trips!",
     redirect: "/",
     description: "Trips is good",
-    image:
-      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+    image: "/sortvisualizer.webp"
   },
   {
     name: "Trips!",
     redirect: "/",
     description: "Trips is good",
-    image:
-      "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+    image: "/wwwallet.webp"
   }
 ] as const;
 </script>
